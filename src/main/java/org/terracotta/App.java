dc7fcb8e64c959c0118919c9933ea9ca91de0dc5
Vladimir Katson <vkatson@griddynamics.com>
2009-02-09 18:10:31 +0300

    comments added

--- src/main/java/org/terracotta/App.java
+++ src/main/java/org/terracotta/App.java
@@ -12,8 +12,12 @@ import javax.management.MalformedObjectNameException;
 public class App {
     public static void main(String[] args) throws InterruptedException, MalformedObjectNameException {
 
+        //Creating listener istance.
         ClusterEvents.ClusterListener listener = new ClusterEvents.ClusterListener();
+        //Registering listner.
         ClusterEvents.registerListener(listener);
+        //Now we're waiting for listener registration completion. Here the program hangs up.
+        //If we skip this step listener.getMyNodeId() would return null istead of nodeID.
         listener.waitForRegistration();
 
         System.out.println("+++++++++++++++++++++++++++++++++++++");
